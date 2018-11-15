51c6dd314b6989573c610de5fdc67ee67606a5b0
nahkranoth <joey@nahkranoth.nl>
2018-11-16 00:15:35 +0100

    Added small change

--- src/map/map.js
+++ src/map/map.js
@@ -1,7 +1,3 @@
-import {_} from 'underscore';
-import {MapChunk} from './mapChunk.js';
-import {TileData} from '../data/tileData.js'
-import {MapChunkNeighbour} from './mapChunkNeighbour.js'
 import {MapGenerator} from './mapGenerator.js'
 import {MapDebugController} from "./mapDebugController.js";
 import {MapWorldEntityController} from "./mapWorldEntityController.js";
@@ -14,18 +10,15 @@ export class Map extends Phaser.GameObjects.GameObject {
         this.camera = params.scene.cameras.main;
 
         this.mapChunkController = new MapChunkController({scene:this.scene, map:this});
-
         this.mapGenerator = new MapGenerator({scene:this.scene, map:this});
-
-
         this.mapWorldEntityController = new MapWorldEntityController({scene:this.scene});
-
         this.mapChunkController._activeChunkChanged();
+
         this.mapDebugController = new MapDebugController({enabled:true, scene:this.scene, map:this});
     }
 
     update(){
         this.mapChunkController._updateActiveChunk();
-        this.mapDebugController.update(this);
+        this.mapDebugController.update();
     }
 }
