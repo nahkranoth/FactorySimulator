4d8dea468481eb84e5d64931c4f679faae4545da
nahkranoth <joey@nahkranoth.nl>
2018-11-20 00:40:32 +0100

    Added worldEntity reaction on fireball hit

--- src/map/spriteEntity.js
+++ src/map/spriteEntity.js
@@ -10,19 +10,29 @@ export class SpriteEntity extends Phaser.Physics.Arcade.Sprite{
         this.scene.add.existing(this);﻿
         this.depth = this.y; + TileData.PROPERTIES.TILESIZE + TileData.PROPERTIES.DEPTHSTART - (this.height/2);
 
-        this.scene.physics.add.overlap(this.scene.fireBall, this, ()=>{
-            console.log("Burn Entity");
+        this.scene.physics.add.overlap(this.scene.fireBall, this, (fireBall, spriteEntity)=>{
+            this.burn();
         });
+    }
 
+    burn(){
+        if(this.frame.name == "Tree1" || this.frame.name == "Tree3"){
+            this.setFrame("Tree2");
+        }
+
+        if(this.frame.name == "Deer") {
+            console.log("Deer");
+            this.assignedToWorldEntity.switchBehaviourState("fleeing");
+        }
+    }
+
+    setFrame(name){
+        super.setFrame(name);
     }
 
     reAssign(worldEntity){
         this.assignedToWorldEntity.slumber();
         this.assignedToWorldEntity = worldEntity;
-    }
-
-    setBodySize(frame){
-        this.body.setSize(this.frame.width, this.frame.height);
     }
 
     setPosition(x, y){
