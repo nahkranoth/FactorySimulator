5cfd8906fd0fa767a59d64232d8d7714a8528dee
nahkranoth <joey@nahkranoth.nl>
2018-11-22 20:33:33 +0100

    Removed unused classes

--- src/objects/fireball.js
+++ src/objects/fireball.js
@@ -15,24 +15,11 @@ export class FireBall extends Phaser.Physics.Arcade.Sprite{
         this.body.setVelocityY((Math.random()*20) + 100);
         this.body.setCircle(30, 10, 10);
         this.body.onWall();
-
-        this.flipTimer = 0;
-        this.deltaTime = 0;
-        //this.body.on("collide", () => {console.log("Collide")})
     }
 
     onWorldSpriteCollision(sprite){
         sprite.assignedToWorldEntity.burn();
     }
-
-    // flipVelocity(){
-    //     this.deltaTime = Date.now() - this.flipTimer;
-    //     if(this.deltaTime >= 300){
-    //         //this.body.setVelocityX(-this.body.velocity.x);
-    //         this.body.setVelocityY(-this.body.velocity.y);
-    //         this.flipTimer = Date.now();
-    //     }
-    // }
 
     update(){
         this.angle+=2;
